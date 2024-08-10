c6fc9daec5a26f7fb7a3f54f5985d52b5107db98
fernandomema <fernandomema@gmail.com>
2024-08-10 14:10:52 +0200

    Add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://docs.zumito.dev',
   integrations: [starlight({
     title: 'Zumito Docs',
     social: {
