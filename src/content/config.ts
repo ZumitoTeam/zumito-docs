f57c2bd88ccdf4fb8cd6358af0eea8c004fbaac7
Fernando Merino <fernandomema@gmail.com>
2025-01-21 09:13:51 +0000

    Add schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 import { defineCollection } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { topicSchema } from 'starlight-sidebar-topics-dropdown/schema'
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
+	schema: docsSchema({ extend: topicSchema })
 };
